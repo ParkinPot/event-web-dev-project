1c5cdf4670d5a9729a556ae608ed592e1cc66f02
Ananza008z <anansit1111@gmail.com>
2026-02-17 11:23:34 +0700

    Create MVC
    new MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace event_web_dev_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
